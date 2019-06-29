e307c9d2824da43912d4e3ed04d1d43028667cb7
Taylor-Osbourne <Taylor.osbourne@coxautoinc.com>
2019-06-29 14:19:17 -0400

    added case to check for style pref

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 `$  git clone  https://github.com/taylorosbourne/create-react-component.git`
 
-`$ chmod  755 ./create-react-component/create-react-component`
+`$ chmod  755 ./create-react-component/create-react-component.sh`
 
-`$ alias crc='./create-react-component/create-react-component'`
+`$ alias crc='./create-react-component/create-react-component.sh'`
+
+
+<!-- git fetch https://github.com/taylorosbourne/create-react-component.git -->
+
+<!-- git checkout FETCH_HEAD -- ./create-react-component.sh -->
+
+<!-- chmod 755 ./create-react-component.sh -->
+
+<!-- alias create-react-component='./create-react-component.sh' -->
--- create-react-component.sh
+++ create-react-component.sh
@@ -2,6 +2,7 @@
 
 SRC="src"
 COMPONENTS="components"
+
 if [ -e "$SRC" ]; then
     cd "$SRC"
 else
@@ -13,6 +14,7 @@ if [ -e "$COMPONENTS" ]; then
     cd "$COMPONENTS"
 else
     mkdir "$COMPONENTS"
+    echo "  create    components"
     cd "$COMPONENTS"
 fi
 
@@ -20,11 +22,36 @@ COMPONENTNAME=${1?Error: Enter a name for your component}
 
 mkdir $COMPONENTNAME
 
+echo "  create    $COMPONENTNAME"
+
 cd $COMPONENTNAME
 
-touch $COMPONENTNAME-styles.js
+read -p "   Select a style type (css, sass, styled-components) " ANSWER
 
-echo "// import styled from 'styled-components'" >$COMPONENTNAME-styles.js
+case "$ANSWER" in
+[cC][sS][sS])
+    touch $COMPONENTNAME.css
+    echo "  create    $COMPONENTNAME.css"
+    ;;
+[sS][tT][yY][lL][eE][dD][-][cC][oO][mM][pP][oO][nN][eE][nN][tT][sS])
+    touch $COMPONENTNAME-styles.js
+    echo "  create    $COMPONENTNAME-styles.js"
+    echo "import styled from 'styled-components'
+
+export {} " >$COMPONENTNAME-styles.js
+    ;;
+[sS][aA][sS][sS])
+    touch $COMPONENTNAME.scss
+    echo "  create    $COMPONENTNAME.scss"
+    ;;
+*)
+    echo "Please select a style type"
+    ;;
+esac
+
+touch $COMPONENTNAME.jsx
+
+echo "  create    $COMPONENTNAME.jsx"
 
 echo "import React from 'react';
 
@@ -40,7 +67,9 @@ export default $COMPONENTNAME;" >$COMPONENTNAME.jsx
 
 touch $COMPONENTNAME.test.js
 
-echo  "import React from 'react';
+echo "  create    $COMPONENTNAME.test.js"
+
+echo "import React from 'react';
 import ReactDOM from 'react-dom';
 import $COMPONENTNAME from './$COMPONENTNAME';
 
@@ -52,22 +81,17 @@ it('renders without crashing', () => {
 
 touch package.json
 
+echo "  create    packaga.json"
+
 echo "{
     \"main\":  \"$COMPONENTNAME.jsx\"
 }" >package.json
 
 cd "../../../"
 
-echo "$COMPONENTNAME Component created"
+echo "
+  $COMPONENTNAME Component created"
 
-# Sass
-
-# touch $COMPONENTNAME.scss
-
-# Comment this out on  line 25-27
-# touch $COMPONENTNAME-styles.js
-
-# echo "// import styled from 'styled-components'" >$COMPONENTNAME-styles.js
 
 # This  will continually add with each new component, no good
 
